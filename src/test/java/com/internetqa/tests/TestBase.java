33302bd31a1e563c38d611e4c3100a257f8f1db3
Shmahailo <yulifeya@ukr.netgit cleargit config --global user.email yulifeya@ukr.net>
2025-07-15 22:19:53 +0200

    add listener

--- src/test/java/com/internetqa/tests/TestBase.java
+++ src/test/java/com/internetqa/tests/TestBase.java
@@ -1,25 +1,38 @@
 package com.internetqa.tests;
 
+import com.internetqa.config.ApplicationManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 
-    public class TestBase {
-        WebDriver driver;
-        @BeforeMethod
-        public void init(){
-            driver = new ChromeDriver();
-            driver.get("https://the-internet.herokuapp.com/");
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        }
-        @AfterMethod(enabled = false)
-        public void tearDown(){
-            driver.quit();
-        }
+public class TestBase {
+    protected ApplicationManager app = new ApplicationManager(System.getProperty("browser","chrome"));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void init(Method method){
+        driver= app.startTest();
+        logger.info("Start test: " + method.getName());
     }
+    @AfterMethod(enabled = true)
+    public void tearDown(ITestResult result){
+        if (result.isSuccess()){
+            logger.info("Test result: Passed" + result.getMethod().getMethodName());
+        }else {
+            logger.error("Test result: Failed" + result.getMethod().getMethodName());
+        }
+        app.stopTest();
+    }
+}
+
+
 
